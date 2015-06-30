1bbdb52468c9eb049e753a1a1d1258453d714b1e
Yusuf Ayuba <virbo80@yahoo.com>
2015-06-30 21:53:47 +0800

    add news module ref_status

---
+++ application/controllers/ref_status.php
@@ -0,0 +1,97 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * WS Client Feeder Status Mahasiswa Module
+ * 
+ * @author      Yusuf Ayuba
+ * @copyright   2015
+ * @Url         http://jago.link
+ * @Github      https://github.com/virbo/wsfeeder
+ * 
+ */
+ 
+class Ref_status extends CI_Controller {
+        
+    private $ws_client;
+    private $temp_proxy;
+    private $limit;
+    private $filter;
+    private $order;
+    private $offset;
+    private $tabel;
+    
+    public function __construct()
+    {
+        parent::__construct();
+        if (!$this->session->userdata('login')) {
+            redirect('ws');
+        } else {
+            $this->ws_client = new nusoap_client($this->session->userdata('ws'), true);
+            $this->temp_proxy = $this->ws_client->getProxy();
+            $this->limit = $this->config->item('limit');
+            $this->filter = $this->config->item('filter');
+            $this->order = $this->config->item('order');
+            $this->offset = $this->config->item('offset');
+            $this->tabel = 'status_mahasiswa';
+            $this->load->model('m_feeder','feeder');
+        }
+    }
+    
+    public function index()
+    {
+        $this->view();
+    }
+    
+    public function view($offset=0)
+    {
+        $temp_dir = $this->temp_proxy->GetDictionary($this->session->userdata('token'), $this->tabel);
+        
+        $temp_status = $this->input->post('nm_status');
+        if ($temp_status=='') {
+            
+            $temp_rec = $this->temp_proxy->GetRecordset($this->session->userdata('token'), 
+                                                        $this->tabel, $this->filter, 
+                                                        $this->order, $this->limit, 
+                                                        $offset
+                                                     );
+            $temp_count = $this->feeder->count_all($this->session->userdata('token'), $this->tabel,$this->filter);
+            
+            $temp_jml = $temp_count['result'];
+            
+            $data['temp_status'] = '';
+        } else {
+            $filter_status = "nm_stat_mhs like '%".$temp_status."%'";
+            $temp_rec = $this->feeder->getrset($this->session->userdata('token'), 
+                                                            $this->tabel, $filter_status, 
+                                                            $this->order, $this->limit, 
+                                                            $offset
+                                                         );
+            $temp_count = $this->feeder->count_all($this->session->userdata('token'), $this->tabel,$filter_status);
+            $temp_jml = $temp_count['result'];
+            
+            $data['temp_status'] = $temp_status;
+        }
+
+        //pagination
+        $config['base_url'] = site_url('ref_status/view');
+        $config['total_rows'] = $temp_count['result'];
+        $config['per_page'] = $this->limit;
+        $config['uri_segment'] = 3;
+        $this->pagination->initialize($config);
+        //
+        $data['pagination'] = $this->pagination->create_links();
+        $data['offset'] = $offset;
+        $data['listsdic'] = $temp_dir['result'];
+        $data['listsrec'] = $temp_rec['result'];
+        $data['total'] = $temp_count['result'];
+        
+        $offset==0? $start=$this->pagination->cur_page: $start=$offset+1;
+        $data['start'] = $start;
+        $data['end'] = $this->pagination->cur_page * $this->pagination->per_page;
+        
+        tampil('ref/__view_status',$data);
+    }
+}
+
+/* End of file ref_status.php */
+/* Location: ./application/controllers/ref_status.php */
---
+++ application/views/tpl/ref/__view_status.php
@@ -0,0 +1,82 @@
+<div class="page-header" style="margin-top: 10px;" >
+    <div class="row">
+        <div class="col-md-4">
+            <h4>Daftar Status Mahasiswa</h4>
+        </div>
+        <div class="col-md-8">
+            
+        </div>    
+    </div>
+    
+</div>
+<div class="row">
+    <div class="col-md-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <?php
+                    $attributes = array('class' => 'form-inline');
+                    echo form_open('ref_status/view',$attributes);
+                ?>
+                    <div class="input-group">
+                        <input type="text" class="form-control" name="nm_status" placeholder="Search..." value="<?php echo $temp_status;?>">
+                        <span class="input-group-btn">
+                            <button class="btn btn-success" type="submit"><i class="fa fa-search search-btn"></i></button>
+                        </span>
+                    </div>
+                </form>
+            </div>
+            <div class="panel-body">
+                <table class="table table-hover table-striped table-bordered">
+                    <thead>
+                        <tr>
+                            <th width="5%">#</th>
+                            <?php
+                                foreach ($listsdic as $value) {
+                                    echo "<th>".$value['column_name']."</th>";
+                                }
+                            ?>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            if (!$listsrec) {
+                                echo "<tr>
+                                            <td colspan=\"10\">
+                                                Data Kosong
+                                            </td>
+                                      </tr>";
+                            } else {
+                                $i=0;
+                                foreach ($listsrec as $value) {
+                                    echo "<tr>
+                                                <td>".++$i."</td>";
+                                                foreach ($listsdic as $value2) {
+                                                    if (isset($value[$value2['column_name']])) {
+                                                        $temp_isi = $value[$value2['column_name']];
+                                                    } else {
+                                                        $temp_isi = "";
+                                                    }
+                                                    echo "<td>".$temp_isi."</td>";
+                                                }
+                                       echo "</tr>";
+                                } 
+                            }        
+                        ?>
+                    </tbody>
+                </table> 
+            </div>
+            <div class="panel-footer">
+                <div class="row">
+                    <div class="col-md-6">
+                        <?php
+                            echo "Showing ".$start.' - '.$end.' of '.$total.' results';
+                        ?>
+                    </div>
+                    <div class="col-md-6" style="margin-top: -15px;" >
+                        <?php echo $pagination;?>
+                    </div>
+                </div>
+            </div>
+        </div>       
+    </div>
+</div>
